28e28f9459476f365d6a9fd211548d6d616089b0
TFX0019 <1400kss@gmail.com>
2019-05-17 06:54:30 -0400

    responsive

--- js/custom.js
+++ js/custom.js
@@ -10,4 +10,80 @@ $(document).ready(function() {
             $('.logo').removeClass('logo_fixed');
         }
     });
-})
+
+    var pm = {
+        zona: document.querySelector(".bg_lin2"),
+        figuras: document.querySelectorAll(".bg_lin2 figure"),
+        mouseX: 0,
+        mouseY: 0 
+    }
+
+    var mm = {
+        mouseInit: function() {
+            pm.zona.addEventListener("mousemove", mm.movimientoMouse);
+            for (let i = 0; i < pm.figuras.length; i++) {
+                pm.figuras[i].innerHTML = '<img src="images/bg/linea fondo pag2.svg">';
+                
+            }
+            
+        },
+        movimientoMouse: function(mouse) {
+            // console.log(mouse.offsetX, mouse.offsetY)
+            pm.mouseX = mouse.offsetX;
+            pm.mouseY = mouse.offsetY;
+            for (let i = 0; i < pm.figuras.length; i++) {
+                pm.figuras[i].style.left = pm.mouseX/100+"%"
+                pm.figuras[i].style.top = pm.mouseX / 100 + "%"
+
+            }
+        }
+    }
+
+var ps = {
+    posicionScroll: 0,
+    botonera: document.querySelectorAll('.navbar-nav .nav-item .nav-link'),
+    ruta: null,
+    intervalo: null,
+    destinoScroll: 0
+}
+
+var ms = {
+    scrollInit: function() {
+        for (let i = 0; i < ps.botonera.length; i++) {
+            ps.botonera[i].addEventListener('click', ms.desplazamiento)
+            
+        }
+    },
+    desplazamiento: function (ruta) {
+        ruta.preventDefault();
+        ps.ruta = ruta.target.getAttribute('href');
+        ps.destinoScroll = document.querySelector(ps.ruta).offsetTop;
+        ps.intervalo = setInterval(() => {
+            if (ps.posicionScroll < ps.destinoScroll) {
+                ps.posicionScroll += 100
+                if(ps.posicionScroll >= ps.destinoScroll){
+                    ps.posicionScroll = ps.destinoScroll
+                    clearInterval(ps.intervalo);
+                }
+            } else {
+                ps.posicionScroll -= 100
+                if (ps.posicionScroll <= ps.destinoScroll) {
+                    ps.posicionScroll = ps.destinoScroll
+                    clearInterval(ps.intervalo);
+                }
+            }
+            
+            window.scrollTo(0, ps.posicionScroll);
+        }, 50);
+    }
+}
+
+// hover portafolio
+var pop = {
+    itemP: document.querySelector('.card_item_p'),
+    overlayP: document.querySelector('.overlay_p'),
+    state: false
+}
+mm.mouseInit();
+ms.scrollInit();
+});
